4f66ec663625f5a9fecbc6ebaf59d875c984c18f
ashishb888 <ashish.bhosle008@gmail.com>
2016-07-29 19:35:52 +0530

    Added hw back button service.

---
+++ www/app/util/hwbackbtn.service.js
@@ -0,0 +1,50 @@
+(function() {
+  angular.module('starter').service('hwBackBtnService', hwBackBtnService);
+  hwBackBtnService.$inject = ['$ionicPlatform', 'utilService', '$cordovaToast', '$timeout'];
+
+  function hwBackBtnService($ionicPlatform, utilService, $cordovaToast, $timeout) {
+    var logger = utilService.getLogger();
+    this.HWBackbtnDeregister = undefined;
+
+    this.disableHWBackBtn = function() {
+      logger.debug("disableHWBackBtn() service");
+      this.HWBackbtnDeregister = $ionicPlatform.registerBackButtonAction(function(e) {
+        e.preventDefault();
+        return false;
+      }, 100);
+    }
+
+    this.enableHWBackBtn = function() {
+      logger.debug("enableHWBackBtn() service");
+      if (this.HWBackbtnDeregister !== undefined) {
+        this.HWBackbtnDeregister();
+        this.HWBackbtnDeregister = undefined;
+      }
+    }
+
+    this.tapToExit = function() {
+      logger.debug("tapToExit() service");
+
+      var backBtnCnt = 0;
+
+      $ionicPlatform.registerBackButtonAction(function(e) {
+        if (backBtnCnt == 0) {
+          backBtnCnt++;
+          $cordovaToast.showShortBottom("Press back again to exit app").then(function(success) {
+            logger.debug("cordovaToast: " + success);
+          }, function(error) {
+            logger.error("cordovaToast: " + error);
+          });
+
+          $timeout(function() {
+            backBtnCnt = 0;
+          }, 2000);
+        }else {
+          navigator.app.exitApp();
+        }
+      }, 100);
+    }
+
+    return this;
+  }
+})();
